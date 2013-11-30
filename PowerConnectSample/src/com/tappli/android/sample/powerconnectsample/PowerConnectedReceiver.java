164a997591850b1624cf13e16fe210173a79cd43
htl21kozaiku <htl21kozaiku@gmail.com>
2013-11-30 11:28:02 +0900

    01

---
+++ PowerConnectSample/src/com/tappli/android/sample/powerconnectsample/PowerConnectedReceiver.java
@@ -0,0 +1,19 @@
+package com.tappli.android.sample.powerconnectsample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class PowerConnectedReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if (Intent.ACTION_POWER_CONNECTED.equals(intent.getAction())) {
+			Toast.makeText(context, "Power connected.", Toast.LENGTH_SHORT).show();
+		}
+		if (Intent.ACTION_POWER_DISCONNECTED.equals(intent.getAction())) {
+			Toast.makeText(context, "Power disconnected.", Toast.LENGTH_SHORT).show();
+		}
+	}
+}
