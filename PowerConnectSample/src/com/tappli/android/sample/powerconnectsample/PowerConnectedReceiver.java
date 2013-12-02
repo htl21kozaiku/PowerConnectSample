c1bdd8eeb426d970bbe543b72c334a37dc13330e
GTSJ004 <GTSJ004@GTSJ004-PC>
2013-12-02 19:18:02 +0900

    03

--- PowerConnectSample/src/com/tappli/android/sample/powerconnectsample/PowerConnectedReceiver.java
+++ PowerConnectSample/src/com/tappli/android/sample/powerconnectsample/PowerConnectedReceiver.java
@@ -3,7 +3,6 @@ package com.tappli.android.sample.powerconnectsample;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.Toast;
 
 public class PowerConnectedReceiver extends BroadcastReceiver {
@@ -11,11 +10,10 @@ public class PowerConnectedReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if (Intent.ACTION_POWER_CONNECTED.equals(intent.getAction())) {
-			Toast.makeText(context, "Power connected.", Toast.LENGTH_SHORT).show();
-			 Log.d("DEBUG", "Power connected..."); 
+			Toast.makeText(context, "Power connected!!!!!.", Toast.LENGTH_SHORT).show();
 		}
 		if (Intent.ACTION_POWER_DISCONNECTED.equals(intent.getAction())) {
-			Toast.makeText(context, "Power disconnected.", Toast.LENGTH_SHORT).show();
+			Toast.makeText(context, "Power disconnected!!!!!", Toast.LENGTH_SHORT).show();
 		}
 	}
 }
