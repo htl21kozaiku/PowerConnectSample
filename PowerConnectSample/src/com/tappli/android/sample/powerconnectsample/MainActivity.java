423f537cb59e1e5f83b367e225556c67979bfe7a
GTSJ004 <GTSJ004@GTSJ004-PC>
2013-12-03 15:06:10 +0900

    06

--- PowerConnectSample/src/com/tappli/android/sample/powerconnectsample/MainActivity.java
+++ PowerConnectSample/src/com/tappli/android/sample/powerconnectsample/MainActivity.java
@@ -1,22 +1,40 @@
 package com.tappli.android.sample.powerconnectsample;
 
-import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.view.Menu;
 
+@SuppressLint("NewApi")
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		getFragmentManager().beginTransaction()
+        .replace(android.R.id.content, new MyPrefsFragment()).commit();
 	}
 
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@SuppressLint("NewApi")
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	  public static class MyPrefsFragment extends PreferenceFragment {
+		    @Override
+		    public void onCreate(Bundle savedInstanceState) {
+		      super.onCreate(savedInstanceState);
+		      addPreferencesFromResource(R.xml.prefs);
+		    }
+		  }
+
 
 }
