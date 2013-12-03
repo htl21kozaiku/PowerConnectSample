1260762040d872a79f4ca70d779d693c9c7d7e8b
GTSJ004 <GTSJ004@GTSJ004-PC>
2013-12-03 14:47:28 +0900

    ０５

--- PowerConnectSample/gen/com/tappli/android/sample/powerconnectsample/R.java
+++ PowerConnectSample/gen/com/tappli/android/sample/powerconnectsample/R.java
@@ -8,16 +8,44 @@
 package com.tappli.android.sample.powerconnectsample;
 
 public final class R {
+    public static final class array {
+        public static final int entries_list_preference=0x7f050000;
+        public static final int entryvalues_list_preference=0x7f050001;
+    }
     public static final class attr {
+    }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int action_settings=0x7f090000;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int activity_main=0x7f030000;
+        public static final int main=0x7f030001;
+    }
+    public static final class menu {
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int action_settings=0x7f070003;
+        public static final int app_name=0x7f070001;
+        public static final int hello=0x7f070000;
+        public static final int hello_world=0x7f070004;
+        public static final int title_activity_main=0x7f070002;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
+        public static final int prefs=0x7f040001;
     }
 }
